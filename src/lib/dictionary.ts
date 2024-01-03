f926b497ef59443628081a244a6b990b57af9a83
Eugene_dev <lointainyyy@gmail.com>
2024-01-03 15:12:39 +0200

    update types

--- src/lib/dictionary.ts
+++ src/lib/dictionary.ts
@@ -1,8 +1,10 @@
 import 'server-only'
+import { Locale } from '@/i18n.config'
+
 
 const dictionaries = {
     en: () => import('./dictionaries/en.json').then((module) => module.default),
     ua: () => import('./dictionaries/ua.json').then((module) => module.default),
 }
 
-export const getDictionary = async (locale) => dictionaries[locale]()
+export const getDictionary = async (locale: Locale) => dictionaries[locale]()
