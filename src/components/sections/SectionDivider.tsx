b7c9582285a9c7551833458307d0b8a57725e5ae
Eugene_dev <lointainyyy@gmail.com>
2024-01-05 11:42:20 +0200

    update styles

--- src/components/sections/SectionDivider.tsx
+++ src/components/sections/SectionDivider.tsx
@@ -7,7 +7,7 @@ import { motion } from 'framer-motion';
 function SectionDivider() {
 	return (
 		<motion.div
-			className="bg-gray-200 dark:bg-opacity-20 my-20 h-16 w-1 rounded-full sm:block"
+			className="bg-gray-200 dark:bg-opacity-20 mt-[12.5rem] mb-20 h-16 w-1 rounded-full sm:block"
 			initial={{ opacity: 0, y: 100 }}
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ delay: 0.225 }}
