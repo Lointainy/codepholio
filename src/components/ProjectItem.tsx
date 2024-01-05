8b46704f0682e67df26c1646d3ba3136d3a6d14d
Eugene_dev <lointainyyy@gmail.com>
2024-01-05 19:55:05 +0200

    update animation

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -14,9 +14,9 @@ function ProjectItem({ title, description, tags, imageUrl, links }: ProjectItemT
 	const ref = useRef<HTMLElement>(null);
 	const { scrollYProgress } = useScroll({
 		target: ref,
-		offset: ['0 1', '1.33 1']
+		offset: ['0 1', '0.6 1']
 	});
-	const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+	const scaleProgess = useTransform(scrollYProgress, [0, 0.5], [0.8, 1]);
 	const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
 	return (
